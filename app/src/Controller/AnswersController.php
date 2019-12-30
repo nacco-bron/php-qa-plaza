351df02ce4109b0addad5d44ffc3233c95c10e74
nacco-bron <nacco.bron@gmal.com>
2019-12-31 07:03:15 +0900

    Refactor: 移植後不要になったメソッドを削除

--- app/src/Controller/AnswersController.php
+++ app/src/Controller/AnswersController.php
@@ -25,34 +25,6 @@ class AnswersController extends AppController
     }
 
     /**
-     * 回答投稿処理
-     *
-     * @return \Cake\Http\Response|null 解答投稿後に質問詳細画面に遷移する
-     */
-    public function add()
-    {
-        $answer = $this->Answers->newEntity($this->request->getData());
-        $count = $this->Answers
-                ->find()
-                ->where(['question_id' => $answer->question_id])
-                ->count();
-
-        if ($count >= Answer::ANSWER_UPPER_LIMIT) {
-            $this->Flash->error('回答の上限数に達しました');
-            return $this->redirect(['controller' => 'Questions', 'action' => 'view', $answer->question_id]);
-        }
-        $answer->user_id = 1; // @TODO ユーザ管理機能実装後に修正する
-
-        if ($this->Answers->save($answer)) {
-            $this->Flash->success('回答を投稿しました');
-        } else {
-            $this->Flash->error('回答の投稿に失敗しました');
-        }
-
-        return $this->redirect(['controller' => 'Questions', 'action' => 'view', $answer->question_id]);
-    }
-
-    /**
      * Delete method
      *
      * @param string|null $id Answer id.
