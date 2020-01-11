d1764fc60bb9148550a119e2a1ee845394a3ead2
nacco-bron <nacco.bron@gmal.com>
2020-01-12 05:11:02 +0900

    Authコンポーネント追加

--- app/src/Controller/AppController.php
+++ app/src/Controller/AppController.php
@@ -46,10 +46,34 @@ class AppController extends Controller
         ]);
         $this->loadComponent('Flash');
 
-        /*
-         * Enable the following component for recommended CakePHP security settings.
-         * see https://book.cakephp.org/3.0/en/controllers/components/security.html
-         */
-        //$this->loadComponent('Security');
+        $this->loadComponent('Auth',[
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'username',
+                        'password' => 'password'    
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'Login',
+                'action' => 'index'
+            ],
+            'loginRedirect' => [
+                'controller' => 'Questions',
+                'action' => 'index'
+            ],
+            'logoutRedirect' => [
+                'controller' => 'Login',
+                'action' => 'index'
+            ],
+            'unauthorizedRedirect' => [
+                'controller' => 'Login',
+                'action' => 'index'
+            ],
+            'authError' => 'ログインが必要です'
+        ]);
+
+        $this->Auth->allow(['display', 'index', 'view']);
     }
 }
